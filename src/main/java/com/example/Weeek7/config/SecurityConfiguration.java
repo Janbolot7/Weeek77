963d0fdde6a64721701255ed517c779af9e13b3b
Janbolot7 <jntrv7.7@gmail.com>
2023-12-06 22:49:51 +0600

    update cors

--- src/main/java/com/example/Weeek7/config/SecurityConfiguration.java
+++ src/main/java/com/example/Weeek7/config/SecurityConfiguration.java
@@ -16,8 +16,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -38,30 +43,36 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
-                .csrf().disable()
-                .cors().disable()
-                .authorizeRequests()
-                .antMatchers("/secured").authenticated()
-                .antMatchers("/info").authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                .and()
-                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling()
-                .authenticationEntryPoint((request, response, authException) -> {
-                    if (authException instanceof DisabledException) {
-                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Подтвердите email");
-                    } else {
-                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Неправильный логин или пароль");
-                    }
-                });
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000, http://localhost:5173", "http://localhost:8081", "http://localhost:8080"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "HEAD", "OPTIONS", "PUT", "PATCH", "DELETE"));
+        config.setMaxAge(3600L);
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("Accept", "Access-Control-Request-Method", "Access-Control-Allow-Origin", "Access-Control-Request-Headers",
+                "Accept-Language", "Authorization", "Content-Type", "Request-Name", "Request-Surname", "Origin", "X-Request-AppVersion",
+                "X-Request-OsVersion", "X-Request-Device", "X-Requested-With"));
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http            .csrf().disable()
+                .cors().and()            .authorizeRequests()
+                .antMatchers("/secured").authenticated()            .antMatchers("/info").authenticated()
+                .anyRequest().permitAll()            .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)            .and()
+                .exceptionHandling()            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and()            .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()            .authenticationEntryPoint((request, response, authException) -> {
+                    if (authException instanceof DisabledException) {                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Подтвердите email");
+                    } else {                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Неправильный логин или пароль");
+                    }            });
 
         return http.build();
     }
