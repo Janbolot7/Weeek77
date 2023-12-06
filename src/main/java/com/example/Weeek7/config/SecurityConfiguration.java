50535fe1a41b838c13788b83848c6df540b43df0
Janbolot7 <jntrv7.7@gmail.com>
2023-12-06 23:07:44 +0600

    update cors 2

--- src/main/java/com/example/Weeek7/config/SecurityConfiguration.java
+++ src/main/java/com/example/Weeek7/config/SecurityConfiguration.java
@@ -17,9 +17,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
@@ -62,7 +62,8 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http            .csrf().disable()
+        http
+                .csrf().disable()
                 .cors().and()            .authorizeRequests()
                 .antMatchers("/secured").authenticated()            .antMatchers("/info").authenticated()
                 .anyRequest().permitAll()            .and()
@@ -73,10 +74,8 @@ public class SecurityConfiguration {
                     if (authException instanceof DisabledException) {                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Подтвердите email");
                     } else {                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Неправильный логин или пароль");
                     }            });
-
         return http.build();
     }
-
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
